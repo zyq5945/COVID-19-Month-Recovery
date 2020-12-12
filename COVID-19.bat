849bbafbc1e3f4a87bb42a9533f929371cf01041
zyq5945 <329587848@qq.com>
2020-12-12 08:13:34 +0800

    修改Readme

--- COVID-19.bat
+++ COVID-19.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dp0
 set data_dir=../COVID-19
-for %%i in (0 10 15 20) do python COVID-19.py %data_dir% ./Stage-%%i %%i
+for %%i in (0 1 5 10 15 20) do python COVID-19.py %data_dir% ./Stage-%%i %%i
 
 for /f "delims=1* delims= " %%t in ('date /T ') do set dt=%%t
 for /f "delims=" %%t in ('time /T ') do set tm=%%t
