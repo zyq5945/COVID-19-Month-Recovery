8467ff1302558f174d75a3c902c91e57cfd8b95b
zyq5945 <329587848@qq.com>
2020-12-28 16:20:52 +0800

    2020/12/28 16:20

--- COVID-19.bat
+++ COVID-19.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 set data_dir=../COVID-19
 for %%i in (0 1 5 10 15 20) do python COVID-19.py %data_dir% ./Stage-%%i %%i
 
-for /f "delims=1* delims= " %%t in ('date /T ') do set dt=%%t
+for /f "tokens=1 delims= " %%t in ('date /T ') do set dt=%%t
 for /f "delims=" %%t in ('time /T ') do set tm=%%t
 git add .
 git commit -m "%dt% %tm%"
